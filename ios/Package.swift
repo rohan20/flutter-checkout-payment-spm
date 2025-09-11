07aacffde8e69c9eef7796e0eb1d1cdb61056a01
rohan20 <rhntnj@gmail.com>
2025-09-11 17:07:03 +0200

    restore SPM-only implementation with frames and apple pay support

---
+++ ios/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "flutter_checkout_payment",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "flutter_checkout_payment", targets: ["flutter_checkout_payment"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/checkout/frames-ios", from: "4.3.8")
+    ],
+    targets: [
+        .target(
+            name: "flutter_checkout_payment",
+            dependencies: [
+                .product(name: "Frames", package: "frames-ios")
+            ],
+            path: "Classes",
+            sources: [
+                "SwiftFlutterCheckoutPaymentPlugin.swift",
+                "FlutterCheckoutPaymentPlugin.m"
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .define("FLUTTER_PLUGIN_REGISTRAR", to: "1")
+            ]
+        )
+    ]
+)
