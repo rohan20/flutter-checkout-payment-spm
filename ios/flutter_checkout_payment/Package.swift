4c9ff8c763769ba9a704d55b43e0ce78eea95571
rohan20 <rhntnj@gmail.com>
2025-09-12 13:38:05 +0200

    refactor(spm): move Package.swift and Classes under ios/flutter_checkout_payment; declare Frames/Risk/EventLogger/Fingerprint deps

--- ios/flutter_checkout_payment/Package.swift
+++ ios/flutter_checkout_payment/Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -12,14 +10,19 @@ let package = Package(
         .library(name: "flutter_checkout_payment", targets: ["flutter_checkout_payment"])
     ],
     dependencies: [
-        // Use Frames via Swift Package Manager (SPM)
-        .package(url: "https://github.com/checkout/frames-ios.git", from: "4.3.8")
+        .package(url: "https://github.com/checkout/frames-ios.git", from: "4.3.8"),
+        .package(url: "https://github.com/checkout/checkout-risk-sdk-ios.git", from: "3.0.4"),
+        .package(url: "https://github.com/checkout/checkout-event-logger-ios-framework.git", from: "1.2.4"),
+        .package(url: "https://github.com/fingerprintjs/fingerprintjs-pro-ios", from: "2.10.0")
     ],
     targets: [
         .target(
             name: "flutter_checkout_payment",
             dependencies: [
-                .product(name: "Frames", package: "frames-ios")
+                .product(name: "Frames", package: "frames-ios"),
+                .product(name: "Risk", package: "checkout-risk-sdk-ios"),
+                .product(name: "CheckoutEventLoggerKit", package: "checkout-event-logger-ios-framework"),
+                .product(name: "FingerprintPro", package: "fingerprintjs-pro-ios")
             ],
             path: "Classes",
             sources: [
